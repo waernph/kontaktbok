b874def0ca4d323ca3e674253059158e8458154f
Philip Waern <waernph@gmail.com>
2025-10-01 12:44:21 +0200

    Lagt till metod för att skriva till json som döps till Contacts.json

--- Contact.cs
+++ Contact.cs
@@ -1,3 +1,6 @@
+using System.IO;
+using System.Text.Json;
+
 public class Contact
 {
     public string Name { get; set; }
@@ -7,7 +10,14 @@ public class Contact
     public string Phone { get; set; }
     public string EMail { get; set; }
 
-    public Contact(string name, string adress, string zipCode, string city, string phone, string eMail)
+    public Contact(
+        string name,
+        string adress,
+        string zipCode,
+        string city,
+        string phone,
+        string eMail
+    )
     {
         Name = name;
         Adress = adress;
@@ -15,5 +25,38 @@ public class Contact
         City = city;
         Phone = phone;
         EMail = eMail;
+    }
+
+    public static void SaveToFile(
+        string Name,
+        string Adress,
+        string ZipCode,
+        string City,
+        string Phone,
+        string EMail
+    )
+    {
+        var contactItem = new
+        {
+            Name,
+            Adress,
+            ZipCode,
+            City,
+            Phone,
+            EMail,
+        };
+        string fileName = "Contacts.json";
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            AllowTrailingCommas = true,
+        };
+        string json = JsonSerializer.Serialize(contactItem, options);
+
+        //Console.WriteLine(json);
+        using (StreamWriter writer = new StreamWriter(fileName, true))
+        {
+            writer.WriteLine(json);
+        }
     }
 }
