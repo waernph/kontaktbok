ea80d59b839ac19d13a72fc95f6730b5142990dd
fladalt <frodedahlman@gmail.com>
2025-10-01 09:50:18 +0200

    la till class contact med props

--- Contact.cs
+++ Contact.cs
@@ -1,1 +1,19 @@
-//Hej!
+public class Contact
+{
+    public string Name { get; set; }
+    public string Adress { get; set; }
+    public string ZipCode { get; set; }
+    public string City { get; set; }
+    public string Phone { get; set; }
+    public string EMail { get; set; }
+
+    public Contact(string name, string adress, string zipCode, string city, string phone, string eMail)
+    {
+        Name = name;
+        Adress = adress;
+        ZipCode = zipCode;
+        City = city;
+        Phone = phone;
+        EMail = eMail;
+    }
+}
